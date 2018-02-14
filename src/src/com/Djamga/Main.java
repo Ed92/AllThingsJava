1001e8fd40b3ee49069ea710904d18ee9a006265
erik djamgarian <djamgarian0702@gmail.com>
2018-02-14 10:02:02 +0100

    Java intro

---
+++ src/src/com/Djamga/Main.java
@@ -0,0 +1,27 @@
+package com.Djamga;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //width of int = 32 (4 bytes)
+        int myIntValue = 5/2;
+
+        //width of float = 32 (4 bytes)
+        float myFloatValue = 5f;
+
+        //width of double 64 (8 bytes)
+        double myDoubleValue = 5d/2d;
+
+        System.out.println("myIntValue = " + myIntValue);
+        System.out.println("myFloatValue = " + myFloatValue);
+        System.out.println("myDoubleValye = " + myDoubleValue);
+
+
+
+            double numPounds = 200d;
+            double convertedKilograms = numPounds * 0.45359237d;
+            System.out.println("Kilograms =" + convertedKilograms);
+
+
+        }
+    }
