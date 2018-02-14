407c9213011fbb30dbbd71c9957b598952ef9b83
erik djamgarian <djamgarian0702@gmail.com>
2018-02-14 10:34:10 +0100

    Java Course

--- CharAndBoolean/src/com/Djamga/Main.java
+++ CharAndBoolean/src/com/Djamga/Main.java
@@ -1,8 +1,18 @@
 package com.Djamga;
 
+
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    char myChar = '\u00A9';
+	    System.out.println("Unicode output was:" + myChar);
+
+
+	    boolean myBoolean = false;
+	    boolean isMale = true;
+
+	    char myChar2 = '\u00AE';
+        System.out.println("Unicode output was: " + myChar2);
     }
 }
